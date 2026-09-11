a230a589425dd3f5c8af1ccd618f4c5689152deb
Marco Stiehl <marco.stiehl.cv@gmail.com>
2026-09-11 11:26:44 +0000

    first commit for module real estate

---
+++ real_estate_mstiehl/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Real Estate MSTIEHL',
+    'version': '19.0.1.0.0',
+    'summary': 'Real Estate MSTIEHL',
+    'description': '',
+    'author': 'Marco Stiehl',
+    'website': '',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'data': [
+        "security/ir.model.access.csv",
+        "views/property.xml",
+        "menu/menu.xml"
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
