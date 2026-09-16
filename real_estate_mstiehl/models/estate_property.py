48a2b9c7deb0ecf8b8e84212bc7c4b0cd0190c0a
marcostiehlcv <marco.stiehl.cv@gmail.com>
2026-09-16 17:45:01 +0200

    fix fields

--- real_estate_mstiehl/models/estate_property.py
+++ real_estate_mstiehl/models/estate_property.py
@@ -1,7 +1,6 @@
 from odoo import models, fields, api, _
 from datetime import datetime
 from datetime import timedelta
-from .res_country import ResCountry, ResCountryState
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -29,8 +28,8 @@ class EstateProperty(models.Model):
     street2 = fields.Char()
     zip = fields.Char(change_default=True)
     city = fields.Char()
-    state_id: ResCountryState = fields.Many2one("res.country.state", string='State', ondelete='restrict', domain="[('country_id', '=?', country_id)]")
-    country_id: ResCountry = fields.Many2one('res.country', string='Country', ondelete='restrict')
+    state_id = fields.Many2one("res.country.state", string='State', ondelete='restrict', domain="[('country_id', '=?', country_id)]")
+    country_id = fields.Many2one('res.country', string='Country', ondelete='restrict')
     country_code = fields.Char(related='country_id.code', string="Country Code")
     partner_latitude = fields.Float(string='Geo Latitude', digits=(10, 7))
     partner_longitude = fields.Float(string='Geo Longitude', digits=(10, 7))
