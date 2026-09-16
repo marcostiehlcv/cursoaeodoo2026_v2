156dbf75395ff03c4d5bf4eb2b4bb43343aee838
marcostiehlcv <marco.stiehl.cv@gmail.com>
2026-09-16 14:06:45 +0200

    fix file name

--- real_estate_mstiehl/__manifest__.py
+++ real_estate_mstiehl/__manifest__.py
@@ -9,12 +9,12 @@
     'depends': ['base', 'mail'],
     "data": [
         "security/ir.model.access.csv",
-        "views/property_listing_type_views.xml",
         "views/estate_listing_views.xml",
+        "views/estate_listing_type_views.xml",
         "views/property_offer_views.xml",
         "views/property_tag_views.xml",
         "views/property_type_views.xml",
-        "views/property_views.xml",        
+        "views/property_views.xml",
         "menu/menu.xml"
     ],
     'installable': True,
