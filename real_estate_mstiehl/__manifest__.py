ada13391dc7043653ab9590abb5f2d620ffc960c
marcostiehlcv <marco.stiehl.cv@gmail.com>
2026-09-16 14:04:31 +0200

    update manifest

--- real_estate_mstiehl/__manifest__.py
+++ real_estate_mstiehl/__manifest__.py
@@ -9,13 +9,12 @@
     'depends': ['base', 'mail'],
     "data": [
         "security/ir.model.access.csv",
-        "views/estate_listing_type_views.xml",
+        "views/property_listing_type_views.xml",
         "views/estate_listing_views.xml",
-        "views/property_category_views.xml",
         "views/property_offer_views.xml",
         "views/property_tag_views.xml",
         "views/property_type_views.xml",
-        "views/property_views.xml",
+        "views/property_views.xml",        
         "menu/menu.xml"
     ],
     'installable': True,
