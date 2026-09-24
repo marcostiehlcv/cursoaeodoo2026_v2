c42612d81907c2596500b69aa62c436716883554
marcostiehlcv <marco.stiehl.cv@gmail.com>
2026-09-24 17:17:36 +0200

    fix: change next_visit_date field type from Char to Datetime for accurate scheduling

--- real_estate_mstiehl/models/estate_listing.py
+++ real_estate_mstiehl/models/estate_listing.py
@@ -37,12 +37,13 @@ class EstateListing(models.Model):
 
     user_id = fields.Many2one(comodel_name="res.users", string="User", default=lambda self: self.env.user)
     
-    next_visit_date = fields.Char(string="Next Visit Date", readonly=True, compute="_compute_next_visit_date", store=True)
-    
+    next_visit_date = fields.Datetime(string="Next Visit Date", readonly=True, compute="_compute_next_visit_date", store=True)
+
     @api.depends('visit_ids')
     def _compute_next_visit_date(self):
         for record in self:
-            next_visit = self.env['estate.property.visit'].search([('listing_id', '=', record.id), ('stage_id_code', '=', 'confirmed'), ('date', '>=', fields.Date.today())], order='date asc', limit=1)
+            next_visit = self.env['estate.property.visit'].search([('listing_id', '=', record.id), ('state', '=', 'confirmed'), ('date', '>=', fields.Date.today())], order='date asc', limit=1)
+            record.next_visit_date = next_visit.date if next_visit else None
 
     @api.depends('contract_ids')
     def _compute_contract_count(self):
